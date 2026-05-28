99e776e68022f3ff5a5bb77dba316deb7ee86fa2
Prince Kahar <kaharprince2006@gmail.com>
2026-05-28 13:45:42 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import LandingPage from './pages/landing';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
-    </>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+        </Routes>
+      </Router>
+    </div>
+
   );
 }
 
