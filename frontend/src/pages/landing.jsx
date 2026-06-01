decb8df45ea136a2327a946a0072f0afb4cc0c94
Prince Kahar <kaharprince2006@gmail.com>
2026-06-01 23:47:30 +0530

    new

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import "../App.css";
 
 function LandingPage() {
     return (
-        <div>
-            <h1>Landing Page</h1>
+        <> <div>
+            <div className="text-black">
+                <h1>KairosMeet</h1>
+            </div>
+            <div className="flex gap-2">
+                <Button>Login</Button>
+                <Button>Sign Up</Button>
+            </div>
         </div>
+            <h1>heelo</h1></>
     );
 }
 
